65caac7f6efc92a439f173ef735c225f92157560
Grant Sander <gksander93@gmail.com>
2019-11-09 16:06:07 -0700

    Increase header text size

--- src/components/GksTitle.vue
+++ src/components/GksTitle.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="flex flex-col items-center p-5 text-primary-900 bg-primary-100">
+  <div class="flex flex-col items-center p-6 text-white bg-primary-800">
     <g-image
       src="~/assets/img/headshot.jpg"
       alt="Grant's headshot"
       class="rounded-full shadow-md"
-      width="125"
-      height="125"
+      width="150"
+      height="150"
       quality="95"
     />
-    <h1 class="text-5xl font-thin">Grant Sander</h1>
-    <h2 class="text-2xl leading-none text-gray-700">Fullstack Developer</h2>
-    <h3 class="text-lg leading-tight text-gray-700">Web and Mobile</h3>
+    <h1 class="text-5xl sm:text-6xl font-thin text-center">Grant Sander</h1>
+    <h2 class="text-3xl sm:text-4xl font-thin leading-none text-gray-300">
+      Fullstack Developer
+    </h2>
+    <h3 class="text-2xl sm:text-3xl font-thin leading-tight text-gray-300">
+      Web and Mobile
+    </h3>
   </div>
 </template>
 
